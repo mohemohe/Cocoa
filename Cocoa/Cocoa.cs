582ff5d239c226ea39209c47d93d15451fb608d0
mohemohe <mohemohe@ghippos.net>
2015-10-25 05:48:12 +0900

    add base

---
+++ Cocoa/Cocoa.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cocoa
+{
+    static class Cocoa
+    {
+        public static void Version()
+        {
+            Console.WriteLine(
+                @"Cocoa v" +
+                Assembly.GetExecutingAssembly().GetName().Version.ToString());
+
+            using (var choco = new ChocolateyHost())
+            {
+                choco.Exec();
+            }
+        }
+
+        public static void Install(IEnumerable<string> packages)
+        {
+            var sb = new StringBuilder();
+            sb.Append(@"install");
+            packages.ToList().ForEach(x => sb.Append(@" " + x));
+
+            using (var choco = new ChocolateyHost())
+            {
+                choco.ExecInteractive(sb.ToString());
+            }
+        }
+
+        public static void Uninstall(IEnumerable<string> packages)
+        {
+            var sb = new StringBuilder();
+            sb.Append(@"uninstall");
+            packages.ToList().ForEach(x => sb.Append(@" " + x));
+
+            using (var choco = new ChocolateyHost())
+            {
+                choco.ExecInteractive(sb.ToString());
+            }
+        }
+
+        public static void Upgrade(IEnumerable<string> packages)
+        {
+            var sb = new StringBuilder();
+            sb.Append(@"upgrade");
+            packages.ToList().ForEach(x => sb.Append(@" " + x));
+
+            using (var choco = new ChocolateyHost())
+            {
+                choco.ExecInteractive(sb.ToString());
+            }
+        }
+    }
+}
---
+++ Cocoa/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cocoa
+{
+    static class Program
+    {
+        static bool ShowVersion;
+        static List<string> InstallPackages = new List<string>();
+        static List<string> UninstallPackages = new List<string>();
+        static List<string> UpgradePackages = new List<string>();
+        static bool IsNoConfirm;
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                Cocoa.Version();
+
+            ParseArgs(args);
+            ExecCocoa();
+        }
+
+        static void ParseArgs(string[] args)
+        {
+            for (var i = 0; i < args.Length; i++)
+            {
+                var arg = args[i].Replace('/', '-');
+
+                switch (arg)
+                {
+                    case @"--version":
+                        ShowVersion = true;
+                        break;
+
+                    case @"-S":
+                    case @"install":
+                        for (i++; i < args.Length && !args[i].StartsWith("-"); i++)
+                        {
+                            InstallPackages.Add(args[i]);
+                        }
+                        i--;
+                        break;
+
+                    case @"-R":
+                    case @"uninstall":
+                        for (i++; i < args.Length && !args[i].StartsWith("-"); i++)
+                        {
+                            UninstallPackages.Add(args[i]);
+                        }
+                        i--;
+                        break;
+
+                    case @"-Su":
+                    case @"upgrade":
+                        for (i++; i < args.Length && !args[i].StartsWith("-"); i++)
+                        {
+                            UpgradePackages.Add(args[i]);
+                        }
+                        i--;
+                        break;
+
+                    case @"-y":
+                    case @"--noconfirm":
+                        IsNoConfirm = true;
+                        break;
+
+                    default:
+                        Console.WriteLine(@"Invalid argument '" + arg + @"'.");
+                        break;
+                }
+            }
+        }
+
+        static void ExecCocoa()
+        {
+            if (UpgradePackages.Count != 0)
+            {
+                if (IsNoConfirm)
+                {
+                    UpgradePackages.Add(@"-y");
+                }
+                Cocoa.Upgrade(UpgradePackages);
+            }
+
+            if (InstallPackages.Count != 0)
+            {
+                if (IsNoConfirm)
+                {
+                    InstallPackages.Add(@"-y");
+                }
+                Cocoa.Install(InstallPackages);
+            }
+
+            if (UninstallPackages.Count != 0)
+            {
+                if (IsNoConfirm)
+                {
+                    UninstallPackages.Add(@"-y");
+                }
+                Cocoa.Uninstall(UninstallPackages);
+            }
+
+            if (ShowVersion)
+            {
+                Cocoa.Version();
+            }
+        }
+    }
+}
