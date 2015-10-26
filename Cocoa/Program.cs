da0cc04357013196f22a883a6553ad8e1048ea60
mohemohe <mohemohe@ghippos.net>
2015-10-27 07:04:40 +0900

    suppress blank line in ChocolateyHost.ExecInteractive()

--- Cocoa/Program.cs
+++ Cocoa/Program.cs
@@ -29,7 +29,7 @@ namespace Cocoa
             ParseArgs(args);
             if (!IsInvalidOperation)
             {
-                ExecCocoa();
+                Task.Run(() => ExecCocoa()).Wait();
             }
         }
 
